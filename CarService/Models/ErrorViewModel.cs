cd44464f0fe4963ffa4bceffcc78c4c9d53604d6
jjtripode@hotmail.com <cisco@FIC.ORG.AR>
2019-08-26 20:36:05 -0300

    se agrego proyecto de TEST

---
+++ CarService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CarServiceFronted.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
